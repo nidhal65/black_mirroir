cd7e5fd1c5e67701903e300bf2f0dfb1f4e1725a
Nidhal Mouldi <nmouldi@mcube.tn>
2018-11-20 23:11:25 +0100

    - créer la page de l'episode - lister les espisodes par saison

--- src/EpisodesBundle/Controller/DefaultController.php
+++ src/EpisodesBundle/Controller/DefaultController.php
@@ -3,16 +3,30 @@
 namespace EpisodesBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="index")
      */
     public function indexAction()
     {
         $service = $this->get('episodes.call_api');
         $resources = $service->getAPI()->resources;
         return $this->render('EpisodesBundle:default:index.html.twig',['resources' => $resources]);
+    }
+
+    /**
+     * @Route("/episode/{id}", name="episode")
+     * @param $id
+     * @return Response
+     */
+    public function episodeAction($id)
+    {
+        $service = $this->get('episodes.call_api');
+        $episode = $service->getEpisode($id);
+        return $this->render('EpisodesBundle:default:episode.html.twig',['episode' => $episode]);
     }
 }
--- src/EpisodesBundle/Services/CallAPI.php
+++ src/EpisodesBundle/Services/CallAPI.php
@@ -17,11 +17,25 @@ class CallAPI
 
     const HEADERS = ['Accept' => 'application/x-www-form-urlencoded', 'X-Auth-Token' => 'TokenADNTest2018'];
 
+    const API_EPISODE = 'http://api.tvmaze.com/episodes/';
+
+    const HEADERS_EPISODE = ['Accept' => 'application/x-www-form-urlencoded'];
+
     /**
      * @return mixed
      */
     public function getAPI(){
         $response = Request::get(self::URL, self::HEADERS);
+        return $response->body;
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function getEpisode($id){
+        $url = self::API_EPISODE.$id;
+        $response = Request::get($url, self::HEADERS_EPISODE);
         return $response->body;
     }
 
